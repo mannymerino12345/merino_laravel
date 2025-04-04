0d31904bcd99e0c65c63102458e3164a9ea001fe
manny merino <mannymerino00@gmail.com>
2025-04-04 12:39:15 +0800

    added ROles table and foreign key to users table

--- database/migrations/2025_04_04_040025_create_roles_table.php
+++ database/migrations/2025_04_04_040025_create_roles_table.php
@@ -11,14 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->unsignedBigInteger('role_id');
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('role_name')->unique();
+            $table->string('url');
             $table->timestamps();
         });
     }
@@ -28,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('_roles');
     }
 };
